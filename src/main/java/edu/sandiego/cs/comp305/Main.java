a5a1056a0df79b5aed08d14a9e9bd73148fb9ce5
Angie Jeong <ajeong@sandiego.edu>
2026-04-27 01:12:41 -0700

    Fix checkstyle issues

--- src/main/java/edu/sandiego/cs/comp305/Main.java
+++ src/main/java/edu/sandiego/cs/comp305/Main.java
@@ -1,14 +1,10 @@
 package edu.sandiego.cs.comp305;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     private Main() {
 
     }
-
     public static void main(final String[] args) {
-
         System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
